9d1a707162c3c565032bd273fe090f666557ea27
YashBansal7 <56682061+YashBansal7@users.noreply.github.com>
2020-06-20 21:21:35 +0530

    Add files via upload

---
+++ Animation.js
@@ -0,0 +1,10 @@
+var child;
+var father;
+
+
+
+function draw(){
+    child=createSprite(displayWidth/2-100,displayHeight/2,50,50);
+    father=createSprite(displayWidth,displayHeight/2);
+    father.velocityX=-3;
+}
---
+++ Child.js
@@ -0,0 +1,28 @@
+class Child{
+
+    constructor(x,y,width,height) {
+        var options = {
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("sprites/Child.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, 200, 200);
+        pop();
+
+        if (father.body.position.x <500){
+          World.remove(world,this.body);
+        }
+      }
+
+
+}
